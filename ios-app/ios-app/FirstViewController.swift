a92d1fa303c8fe86122481fc8695d160e3b4b25d
Antoine Richard <arichard@restlet.com>
2016-10-25 22:45:38 +0200

    added skeleton for swift app

---
+++ ios-app/ios-app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ios-app
+//
+//  Created by Antoine Richard on 25/10/16.
+//  Copyright © 2016 Antoine Richard. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-app/ios-app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ios-app
+//
+//  Created by Antoine Richard on 25/10/16.
+//  Copyright © 2016 Antoine Richard. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
